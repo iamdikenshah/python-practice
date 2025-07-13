8a0be1885a4e6dc6c858feb70009604b68c1d140
Diken Shah <shah.diken@gmail.com>
2025-07-13 13:12:04 +0530

    Pushed user input code

---
+++ user-input/main.py
@@ -0,0 +1,15 @@
+
+
+# name = input("Enter your name: ")
+# print(f"Hello, {name}!")
+
+# age = int(input("Enter your age: "))
+# print(f"You are {age} years old.")
+
+
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+addition = int(num1) + int(num2)
+print(f"The sum of {num1} and {num2} is {addition}.")
