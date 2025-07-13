81cbce19b098522a7f9e8e94c2206b2b8dd3aead
Diken Shah <shah.diken@gmail.com>
2025-07-13 19:14:24 +0530

    Pushed operators and comments

---
+++ comments/main.py
@@ -0,0 +1,14 @@
+# Single line comment
+
+
+"""
+Multiline comment in Python
+POjisdhasidhasi defdssad
+sdkasldkasldkas
+sdkasdjsakdj
+"""
+
+print("Hello\nWorld!")
+print("This is a tab\tcharacter.")
+
+print("Hello", "World", sep=", ", end="!\n")
---
+++ operators/main.py
@@ -0,0 +1,27 @@
+print(10 + 5) # Output: 15
+print(10 ** 2) # Output: 100
+
+
+print(10 > 5) # Output: True
+print(10 == 5) # Output: False
+
+
+print(True and False) # Output: False
+print(True or False) # Output: True
+print(not True) # Output: False
+
+
+"""
+=
+, += ,
+-=
+,
+*=
+, /= , %= ,
+**=
+, //=.
+
+"""
+x = 10
+x += 5 # Equivalent to x = x + 5
+print(x) # Output: 15
