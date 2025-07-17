6b98f067a24adc7d2220dd0bde03b3306cd44135
Diken Shah <shah.diken@gmail.com>
2025-07-17 09:08:23 +0530

    Pushed minor changes

--- files/main.py
+++ files/main.py
@@ -4,7 +4,6 @@ def read_file():
     try:
         with open('files/diken.txt', 'r') as file:
             content = file.read()
-            file.close()
         return content
     except FileNotFoundError:
         return "File 'diken.txt' not found!"
@@ -16,7 +15,6 @@ def create_and_read_file():
     try:
         with open('files/diken.txt', 'w') as file:
             file.write("Hello, this is Diken's file!\nThis is line 2.")
-            file.close()
     except IOError as e:
         return f"An error occurred while writing to the file: {e}"
 
@@ -24,7 +22,6 @@ def create_and_read_file():
     # Now read the file
     with open('files/diken.txt', 'r') as file:
         content = file.read()
-        file.close()
     return content
 
 create_and_read_file()
@@ -34,7 +31,6 @@ def append_file():
     try:
         with open('files/diken.txt', 'a') as file:
             file.write("\nThis is an appended line.")
-            file.close()
     except IOError as e:
         return f"An error occurred while appending to the file: {e}"
 
@@ -43,3 +39,14 @@ def append_file():
 
 
 print(append_file())
+
+
+def read_lines():
+    try:
+        with open('files/diken.txt', 'r') as file:
+            lines = file.readlines()
+        return lines
+    except FileNotFoundError:
+        return "File 'diken.txt' not found!"
+    
+print(read_lines()) # this return array of lines in the file
